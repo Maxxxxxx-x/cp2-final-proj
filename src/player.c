a61dc3b71a07dad657e15de7f15bf9de93f11930
Anson-YH <yeungyinho@gmail.com>
2024-06-15 03:36:26 +0800

    add player interact with item

---
+++ src/player.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "inventory.h"
+#include "player.h"
+#include "item.h"
+
+Player initPlayer(char *name, Inventory *inventory) {
+    Player player;
+    player.name = name;
+    player.inventory = inventory;
+    player.inventory->items = (Item *)malloc(sizeof(Item) * inventory->maxItems);
+    return player;
+}
+
+void getItem(Player *player, Item *item) {
+    for (int i = 0; i < player->inventory->numItems; i++) {
+        if (strcmp(player->inventory->items[i].name, item->name) == 0){
+            player->inventory->items[i].amount++;
+            return;
+        }
+    }
+    if (player->inventory->numItems < player->inventory->maxItems){
+        player->inventory->items[player->inventory->numItems] = *item;
+        player->inventory->numItems++;
+    }else{
+        printf("Inventory is full!\n");
+    }
+}
+
+void dropItem(Player *player, Item *item) {
+    for (int i = 0; i < player->inventory->numItems; i++){
+        if (strcmp(player->inventory->items[i].name, item->name) == 0){
+            player->inventory->items[i].amount--;
+            if (player->inventory->items[i].amount == 0){
+                for (int j = i; j < player->inventory->numItems - 1; j++){
+                    player->inventory->items[j] = player->inventory->items[j + 1];
+                }
+                player->inventory->numItems--;
+            }
+            break;
+        }
+    }
+}
+
+void useItem(Player *player, Item *item) {
+    for (int i = 0; i < player->inventory->numItems; i++){
+        if (strcmp(player->inventory->items[i].name, item->name) == 0){
+            player->inventory->items[i].amount--;
+            if (player->inventory->items[i].amount == 0){
+                for (int j = i; j < player->inventory->numItems - 1; j++){
+                    player->inventory->items[j] = player->inventory->items[j + 1];
+                }
+                player->inventory->numItems--;
+            }
+            break;
+        }
+        // item function
+    }
+}
+
+void showInventory(Player *player) {
+    int i;
+    printf("Inventory:\n");
+    for (i = 0; i < player->inventory->numItems; i++){
+        printf("%s|%d\n", player->inventory->items[i].name, player->inventory->items[i].amount);
+    }
+}
--- src/player.h
+++ src/player.h
@@ -3,10 +3,17 @@
 #define _PLAYER_H
 
 #include "inventory.h"
+#include "item.h"
 
 typedef struct {
     char *name;
     Inventory *inventory;
 } Player;
+
+Player initPlayer(char *name, Inventory *inventory);
+void getItem(Player *player, Item *item);
+void dropItem(Player *player, Item *item);
+void useItem(Player *player, Item *item);
+void showInventory(Player *player);
 
 #endif
