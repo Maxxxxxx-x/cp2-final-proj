e1e275c86dc73caf512e7aef28b5c8f27150ff7b
maxxxxx <maxxng1028@gmail.com>
2024-06-16 22:56:08 +0800

    feat: main menu

--- src/window.c
+++ src/window.c
@@ -29,23 +29,61 @@ SDL_Texture *background;
 SDL_Texture *character;
 SDL_Texture *dialogue;
 
-
 bool isMainMenuShown = false;
 
 typedef struct {
     const char **items;
     int count;
-} MainMenuOptions;
+} Options;
 
 typedef struct {
-    MainMenuOptions *options;
+    Options *option;
     int selectedIndex;
-} MainMenuState;
+    bool ended;
+} OptionsState;
 
 const char *options[] = {"Start", "Continue", "Load Save", "Options", "Settings"};
-MainMenuOptions mainMenuOptions = {options, 5};
-MainMenuState mainMenuState = {&mainMenuOptions, 0};
+Options mainMenuOptions = {options, 5};
+OptionsState mainMenuState = {&mainMenuOptions, 0};
 
+void handleKeyEvents(OptionsState *state) {
+    if (state->ended != false) {
+        state->ended = false;
+    }
+
+    puts("Handling key inputs...");
+    while (SDL_PollEvent(&event) && state->ended != true) {
+        if (event.type == SDL_KEYDOWN) {
+            printf("%d\n", event.key.keysym.sym);
+            switch (event.key.keysym.sym) {
+            case SDLK_UP:
+                puts("Key Up");
+                if (state->selectedIndex < state->option->count) {
+                    state->selectedIndex += 1;
+                    continue;
+                }
+                state->selectedIndex = 0;
+                break;
+            case SDLK_DOWN:
+                puts("Key Down");
+                if (state->selectedIndex > 0) {
+                    state->selectedIndex -= 1;
+                    continue;
+                }
+                state->selectedIndex = state->option->count;
+                break;
+            case SDLK_RETURN:
+                puts("ENTER");
+                state->ended = true;
+                break;
+            default:
+                puts("Default");
+                break;
+            }
+        }
+    }
+    return;
+}
 
 int createWindow(char *gameName, int width, int height) {
     widthG = width;
@@ -62,7 +100,7 @@ int createWindow(char *gameName, int width, int height) {
     }
 
     titleFont = TTF_OpenFont(NOTO_SANS, 48);
-    font = TTF_OpenFont(NOTO_SANS, 24);
+    font = TTF_OpenFont(NOTO_SANS, 30);
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
         printf("window.h::SDL_Init::%s\n", SDL_GetError());
         return -1;
@@ -87,21 +125,48 @@ int createWindow(char *gameName, int width, int height) {
 
 int renderMainMenu(char *gameName) {
     isMainMenuShown = true;
+
+    SDL_Color green = {57, 128, 36};
     SDL_Color white = {255, 255, 255};
+    SDL_Color gray = {200, 200, 200};
+
     SDL_Surface *surface = TTF_RenderText_Solid(titleFont, gameName, white);
     SDL_Texture *surfaceTexture = SDL_CreateTextureFromSurface(renderer, surface);
 
     SDL_Rect surfaceRect;
     surfaceRect.x = (widthG - surface->w) / 2;
-    surfaceRect.y = 160;
+    surfaceRect.y = 170;
     surfaceRect.w = surface->w;
     surfaceRect.h = surface->h;
 
     SDL_RenderCopy(renderer, surfaceTexture, NULL, &surfaceRect);
     SDL_FreeSurface(surface);
 
-    SDL_SetRenderDrawColor(renderer, 0, 125, 122, 255);
+    SDL_Rect optionRect;
+
+    for (int i = 0; i < mainMenuOptions.count; i++) {
+        optionRect.x = (widthG - TTF_RenderText_Solid(font, mainMenuOptions.items[i], gray)->w) / 2;
+        optionRect.y = 500 + (i * 50);
+        optionRect.w = TTF_RenderText_Solid(font, mainMenuOptions.items[i], gray)->w;
+        optionRect.h = TTF_RenderText_Solid(font, mainMenuOptions.items[i], gray)->h;
+
+        if (i == mainMenuState.selectedIndex) {
+            SDL_SetRenderDrawColor(renderer, green.r, green.g, green.b, 255);
+        } else {
+            SDL_SetRenderDrawColor(renderer, gray.r, gray.g, gray.b, 255);
+        }
+
+        SDL_Surface *optionSurface = TTF_RenderText_Solid(
+            font, mainMenuOptions.items[i], (i == mainMenuState.selectedIndex) ? green : gray);
+        SDL_Texture *optionTexture = SDL_CreateTextureFromSurface(renderer, optionSurface);
+        SDL_RenderCopy(renderer, optionTexture, NULL, &optionRect);
+        SDL_FreeSurface(optionSurface);
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    }
+
     SDL_RenderPresent(renderer);
+    handleKeyEvents(&mainMenuState);
 
     return 0;
 }
@@ -125,36 +190,32 @@ int renderBackground(char *imagePath) {
         return -1;
     }
 
+    SDL_Point imageSize;
     background = IMG_LoadTexture(renderer, imagePath);
-    SDL_QueryTexture(background, NULL, NULL, &widthG, &heightG);
+    SDL_QueryTexture(background, NULL, NULL, &imageSize.x, &imageSize.y);
 
+    printf("Width: %d | Height: %d\n", imageSize.x, imageSize.y);
+
+    float scaleX = (float)(widthG) / (float)(imageSize.x);
+    float scaleY = (float)(heightG) / (float)(imageSize.y);
+    float scale = (scaleX > scaleY) ? scaleX : scaleY;
+
+    int scaledWidth = (int)(imageSize.x * scale);
+    int scaledHeight = (int)(imageSize.y * scale);
+
+    SDL_Rect bgRect;
+    bgRect.x = (widthG / imageSize.x) / 2;
+    bgRect.y = (heightG / imageSize.y) / 2;
+    bgRect.w = scaledWidth;
+    bgRect.h = scaledHeight;
 
     SDL_RenderClear(renderer);
-    SDL_RenderCopy(renderer, background, NULL, NULL);
+    SDL_RenderCopy(renderer, background, NULL, &bgRect);
     SDL_RenderPresent(renderer);
-    printf("Rendered %s as background\n", imagePath);
-    return 0;
-}
+    printf("eendered %s as background\n", imagePath);
+    SDL_DestroyTexture(background);
 
-int renderImage(char *imagePath) {
-    if (!fileExists(imagePath)) {
-        printf("window.h::renderImage::image %s does not exist!\n", imagePath);
-        return -1;
-    }
-    SDL_Texture *image = IMG_LoadTexture(renderer, imagePath);
-    SDL_QueryTexture(image, NULL, NULL, &widthG, &heightG);
-    SDL_RenderClear(renderer);
-    SDL_RenderCopy(renderer, image, NULL, NULL);
-    SDL_RenderPresent(renderer);
     return 0;
-}
-
-void handleKeyboardInput(SDL_Event *event) {
-    switch (event->key.keysym.sym) {
-    case SDLK_ESCAPE:
-        renderPauseMenu();
-        break;
-    }
 }
 
 void handleEvent(Game game) {
@@ -168,7 +229,6 @@ void handleEvent(Game game) {
             exit(EXIT_SUCCESS);
             break;
         case SDL_KEYDOWN:
-            handleKeyboardInput(&event);
             break;
         }
     }
